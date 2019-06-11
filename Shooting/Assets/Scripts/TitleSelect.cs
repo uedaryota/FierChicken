fdaa97307151beee583f170af92993683145e13c
masamx2 <masatakamx2@gmail.com>
2019-06-11 16:04:47 +0900

    Title改良

--- Shooting/Assets/Scripts/TitleSelect.cs
+++ Shooting/Assets/Scripts/TitleSelect.cs
@@ -11,7 +11,14 @@ public class TitleSelect : MonoBehaviour
     }
     public void End()
     {
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
         UnityEngine.Application.Quit();
+#endif
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)) End();
     }
 }
