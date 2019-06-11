b49c51f9e1acdb770ee8ec3d12166477af214889
masamx2 <masatakamx2@gmail.com>
2019-06-11 15:40:37 +0900

    シーン追加　Title GamePlay GameOver Ending

---
+++ Shooting/Assets/Scripts/TitleSelect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSelect : MonoBehaviour
+{
+    public void GamePlaySelect()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+    public void End()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        UnityEngine.Application.Quit();
+    }
+}
